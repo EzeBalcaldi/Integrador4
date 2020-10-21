3de084584c533a06aad6e0440a34513a6e16e469
EzeBalcaldi <ezequielbalcaldi@hotmail.com>
2020-10-21 11:48:29 -0300

    subido

---
+++ Integrador4-master/src/main/java/demo/Entregable4Application.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+* Esta clase permite el inicio de la aplicacion.
+* @author Grupo4
+* @version Octubre 21, 2020
+*/
+
+@SpringBootApplication
+public class Entregable4Application {
+
+		/**
+		* Metodo que corre la app.
+		*/ 
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Entregable4Application.class, args);
+	}
+
+}
